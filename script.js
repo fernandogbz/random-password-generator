3363935f14cc26662ae204e43ce12f7ac9a560be
Fernando Jimenez <fgjv06@gmail.com>
2023-02-28 15:44:54 -0300

    JS - characters and output

---
+++ script.js
@@ -0,0 +1,9 @@
+// ABCDEFGHIJKLMNOPQRSTUVWXWZ
+// abcdefghijklmnopqrstuvwxwz
+// !\"$%&/()=?@~'\\.\';:+=^*_-"
+// 0123456789
+
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXWZabcdefghijklmnopqrstuvwxwz!\"$%&/()=?@~`\\.\;:+=^*_-"0123456789';
+
+let output = document.getElementById("output");
+
